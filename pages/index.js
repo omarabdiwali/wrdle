f9a5da52b7ff393404248af54a0886fe2e6a8c5f
omarabdiwali <omarabdiwali17@gmail.com>
2023-07-17 08:28:43 -0600

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,257 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState, useCallback, useRef } from "react"
+import { words } from 'popular-english-words';
 
-const inter = Inter({ subsets: ['latin'] })
+var allWords = require('an-array-of-english-words')
+let validWords = allWords.filter(d => d.length == 5);
+
+let wrdle = words.getMostPopularLength(1000, 5);
 
 export default function Home() {
+  const [word, setWord] = useState("");
+  const [guess, setGuess] = useState("");
+  const [correct, setCorrect] = useState(false);
+  const [colors, setColors] = useState([]);
+  const [guesses, setGuesses] = useState([]);
+  const [numGuesses, setNumGuesses] = useState(0);
+  const [alphColors, setAlphColors] = useState([]);
+
+  const inpRef = useRef(null);
+
+  const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
+  const keepFocus = useCallback(e => {
+    inpRef.current.focus();
+  }, [])
+
+  const autoFocusFn = useCallback(element => {
+    if (element) {
+      element.focus();
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('blur', keepFocus);
+    return () => {
+      window.addEventListener('blur', keepFocus);
+    }
+  }, [keepFocus])
+
+  useEffect(() => {
+    window.addEventListener("click", keepFocus);
+    return () => {
+      window.addEventListener("click", keepFocus);
+    }
+  }, [keepFocus])
+  
+  useEffect(() => {
+    let gs = [];
+    let cls = [];
+    let cnt = [];
+    let alpColors = [];
+
+    let randomNumber = Math.floor(Math.random() * 1000);
+    let word = wrdle[randomNumber];
+
+    let regText = new RegExp(`^${word}$`);
+    let valid = validWords.filter(d => regText.test(d)).length > 0;
+
+    while (!valid) {
+      randomNumber = Math.floor(Math.random() * 1000);
+      word = wrdle[randomNumber];
+      regText = new RegExp(`^${word}$`);
+      valid = validWords.filter(d => regText.test(d)).length > 0;
+    }
+
+    for (let i = 0; i < 5; i++) {
+      gs.push("     ")
+    }
+
+    for (let i = 0; i < 25; i++) {
+      if (cnt.length == 5) {
+        cls.push(cnt);
+        cnt = []
+      }
+
+      cnt.push("bg-slate-400");
+      alpColors.push("bg-slate-500 text-black");
+    }
+
+    cls.push(cnt);
+    alpColors.push("bg-slate-500 text-black");
+
+    setGuesses(gs);
+    setColors(cls);
+    setAlphColors(alpColors);
+    setWord(word.toUpperCase());
+  }, [])
+
+  const onChange = (e) => {
+    e.preventDefault();
+    let copyGuesses = JSON.parse(JSON.stringify(guesses));
+    let copyColor = JSON.parse(JSON.stringify(colors));
+    
+    let cpyGuess = "";
+    let cpyColor = [];
+
+    let value = e.target.value.trim().toUpperCase();
+    if (value == guess || value.indexOf(" ") > -1) return;
+
+    let alph = new RegExp(`^[A-Za-z]+$`);
+    if (!alph.test(value) && value.length > 0) return;
+
+    if (value.length <= 5) {
+      setGuess(value);
+      let remaining = 5 - value.length;
+      
+      for (let i = 0; i < value.length; i++) {
+        cpyGuess += value[i];
+        cpyColor.push("bg-slate-400 text-black");
+      }
+
+      for (let i = 0; i < remaining; i++) {
+        cpyGuess += " ";
+        cpyColor.push("bg-slate-400");
+      }
+
+      copyGuesses[numGuesses] = cpyGuess;
+      copyColor[numGuesses] = cpyColor;
+      setGuesses(copyGuesses);
+      setColors(copyColor);
+    }
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (guess.length != 5 || correct || numGuesses == 5) return;
+
+    let regText = new RegExp(`^${guess.toLowerCase()}$`);
+    let valid = validWords.filter(d => regText.test(d)).length > 0;
+
+    if (!valid) return;
+
+    let colorArr = [];
+    let remArr = [];
+    let count = 0;
+
+    let uniq = [...word].reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {});
+    
+    let copyColor = JSON.parse(JSON.stringify(colors));
+    let copyGuesses = JSON.parse(JSON.stringify(guesses));
+    let copyAlpColor = JSON.parse(JSON.stringify(alphColors));
+
+    for (let i = 0; i < word.length; i++) {
+      const letter = word[i];
+      const gLetter = guess[i];
+
+      let index = alphabet.indexOf(gLetter);
+
+      if (letter == gLetter) {
+        colorArr.push("bg-green-600 text-black");
+        copyAlpColor[index] = "bg-green-600 text-black";
+        uniq[letter] -= 1;
+        count += 1;
+      } else {
+        colorArr.push("");
+        remArr.push(i);
+      }
+    }
+
+    for (let i = 0; i < remArr.length; i++) {
+      let index = remArr[i];
+      const letter = guess[index];
+
+      let clrIndex = alphabet.indexOf(letter);
+
+      if (uniq[letter] > 0) {
+        if (copyAlpColor[clrIndex] != "bg-green-600 text-black") {
+          copyAlpColor[clrIndex] = "bg-orange-400 text-black";
+        }
+        colorArr[index] = "bg-orange-400 text-black";
+        uniq[letter] -= 1;
+      } else {
+        if (copyAlpColor[clrIndex] == "bg-slate-500 text-black") {
+          copyAlpColor[clrIndex] = "bg-stone-600 text-black";
+        }
+        colorArr[index] = "bg-stone-600 text-black";
+      }
+    }
+
+    if (count == guess.length) setCorrect(true);
+    
+    copyColor[numGuesses] = colorArr;
+    copyGuesses[numGuesses] = guess;
+
+    setColors(copyColor);
+    setGuesses(copyGuesses);
+    setAlphColors(copyAlpColor);
+    setGuess("");
+    setNumGuesses(numGuesses + 1);
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex h-screen">
+      <div className="select-none m-auto">
+        <form onSubmit={onSubmit} className={`opacity-0 ${numGuesses == 5 && !correct ? "hidden" : ""}`}>
+          <input ref={(el)=> {inpRef.current = el; autoFocusFn(el);}} disabled={correct} placeholder="Guess..." type="text" className="bg-inherit" value={guess} onChange={onChange}></input>
+        </form>      
+
+        <center>
+          <div className={`text-xl ${numGuesses == 5 && !correct ? "" : "hidden"}`}>
+            Word was: {word}
+          </div>
+        </center>
+
+        <center>
+          <div className="inline-grid text-white m-5 gap-2 grid-cols-5 text-2xl">
+          {guesses.map((guss, i) => {
+            let letters = guss.split("");
+            let color = colors[i];
+
+            return letters.map((letr, j) => {
+              return <div className={`${color[j]} w-12 h-12 rounded-lg flex`} key={j}>
+                <div className="m-auto">{letr}</div>
+              </div>
+            })
+          })}
+          </div>
+        </center>
+
+        <div className="flex flex-col space-y-4 mt-3 m-auto">
+
+          <div className="inline-grid gap-2 grid-cols-10 m-auto text-2xl">
+            {alphabet.slice(0, 10).map((letter, i) => {
+              let color = alphColors[i];
+
+              return <div className={`${color} w-12 h-12 rounded-lg flex`} key={i}>
+                <div className="m-auto">{letter}</div>
+              </div>
+            })}
+          </div>
+            
+          <div className="inline-grid gap-2 grid-cols-9 m-auto text-2xl">
+          {alphabet.slice(10, 19).map((letter, i) => {
+            i += 10;
+            let color = alphColors[i];
+
+            return <div className={`${color} w-12 h-12 rounded-lg flex`} key={i}>
+              <div className="m-auto">{letter}</div>
+            </div>
+          })}
+          </div>
+
+          <div className="inline-grid gap-2 grid-cols-7 m-auto text-2xl">
+          {alphabet.slice(19).map((letter, i) => {
+            i += 19;
+            let color = alphColors[i];
+
+            return <div className={`${color} w-12 h-12 rounded-lg flex`} key={i}>
+              <div className="m-auto">{letter}</div>
+            </div>
+          })}
+          </div>
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
