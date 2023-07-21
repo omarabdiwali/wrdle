f3b98fc5e41ccd5d65938ab6dc61fdbb5057f102
omarabdiwali <omarabdiwali17@gmail.com>
2023-07-21 08:12:01 -0600

    fixed problem

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,6 @@ export default function Home() {
   const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<<'];
 
   const keepFocus = useCallback(e => {
-    e.preventDefault();
     if (inpRef) {
       inpRef.current.focus({ preventScroll: true });
     }
