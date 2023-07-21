39b1225542497d6f0c41f3a221d7e046080757c6
omarabdiwali <omarabdiwali17@gmail.com>
2023-07-21 08:27:35 -0600

    sizing

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
 
   const [prevWords, setPrevWords] = useState({});
   const [definitions, setDefinitions] = useState([]);
-  const [mobile, setMobile] = useState(true);
+  const [mobile, setMobile] = useState(false);
 
   const inpRef = useRef(null);
   const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<<'];
